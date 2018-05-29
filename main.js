bd5e2f12c82f3fe81d97fdf64a90f802de75d349
SOFe <sofe2038@gmail.com>
2018-05-29 14:26:18 +0800

    Detect page state changes too

--- main.js
+++ main.js
@@ -1,67 +1,77 @@
-(function (document) {
-    var blob = document.getElementsByClassName("blob-wrapper")[0];
-    if (blob === undefined) {
-        return;
-    }
-    var blobWidth = (blob.clientWidth - document.getElementById("L1").clientWidth) * 0.85; // 85% to play safe
-    var lineList = blob.getElementsByClassName("blob-code");
-    var charWidth = (function () {
-        for (var i = 0; i < lineList.length; ++i) {
-            var nodes = lineList[0].childNodes;
-            for (var j = 0; j < nodes.length; ++j) {
-                if (nodes[j] instanceof HTMLElement && nodes[j].innerText.length > 0) {
-                    var node = nodes[j];
-                    return node.getBoundingClientRect().width / node.innerText.length;
-                }
-            }
+(function (document, location) {
+    var prevUrl = "";
+    var func = function () {
+        if (prevUrl === location.pathname) { // ignore line highlighting
+            return;
         }
-        return NaN;
-    })();
-    if (isNaN(charWidth)) {
-        return;
-    }
-    var currentLength;
-    for (var i = 0; i < lineList.length; ++i) {
-        currentLength = 0;
-        breakLine(lineList[i]);
-    }
-    function breakLine(node) {
-        if (node instanceof Text) {
-            var noTabsText = node.textContent.replace(/\t/g, "        ");
-            // actual number of spaces varies with .editorconfig, but let's assume it's 8 here, since we don't care getting too aggressive
-            if (currentLength + charWidth * noTabsText.length >= blobWidth) {
-                if (currentLength > 0) {
-                    node.parentNode.insertBefore(document.createElement("br"), node);
-                    currentLength = 0;
-                }
-                if (charWidth * noTabsText.length >= blobWidth) {
-                    var text = node.textContent;
-                    var tabsCount = text.length - text.replace(/\t/g, "").length; // assume all tabs are at the beginning
-                    while (text.replace(/\t/g, "        ").length * charWidth >= blobWidth) {
-                        var span_1 = document.createElement("span");
-                        var cutLength = Math.floor(blobWidth / charWidth) - tabsCount * 7;
-                        span_1.innerText = text.substr(0, cutLength);
-                        text = text.substr(cutLength);
-                        node.parentNode.insertBefore(span_1, node);
-                        node.parentNode.insertBefore(document.createElement("br"), node);
+        prevUrl = location.pathname;
+        setTimeout(function () {
+            var blob = document.getElementsByClassName("blob-wrapper")[0];
+            if (blob === undefined) {
+                return;
+            }
+            var blobWidth = (blob.clientWidth - document.getElementById("L1").clientWidth) * 0.85; // 85% to play safe
+            var lineList = blob.getElementsByClassName("blob-code");
+            var charWidth = (function () {
+                for (var i = 0; i < lineList.length; ++i) {
+                    var nodes = lineList[0].childNodes;
+                    for (var j = 0; j < nodes.length; ++j) {
+                        if (nodes[j] instanceof HTMLElement && nodes[j].innerText.length > 0) {
+                            var node = nodes[j];
+                            return node.getBoundingClientRect().width / node.innerText.length;
+                        }
                     }
-                    var span = document.createElement("span");
-                    span.innerText = text;
-                    node.parentNode.replaceChild(span, node);
                 }
-                return true;
+                return NaN;
+            })();
+            if (isNaN(charWidth)) {
+                return;
             }
-            currentLength += charWidth * node.textContent.length;
-        }
-        else {
-            var children = [];
-            for (var i = 0; i < node.childNodes.length; ++i) {
-                children.push(node.childNodes[i]);
+            var currentLength;
+            for (var i = 0; i < lineList.length; ++i) {
+                currentLength = 0;
+                breakLine(lineList[i]);
             }
-            for (var i = 0; i < children.length; ++i) {
-                breakLine(children[i]);
+            function breakLine(node) {
+                if (node instanceof Text) {
+                    var noTabsText = node.textContent.replace(/\t/g, "        ");
+                    // actual number of spaces varies with .editorconfig, but let's assume it's 8 here, since we don't care getting too aggressive
+                    if (currentLength + charWidth * noTabsText.length >= blobWidth) {
+                        if (currentLength > 0) {
+                            node.parentNode.insertBefore(document.createElement("br"), node);
+                            currentLength = 0;
+                        }
+                        if (charWidth * noTabsText.length >= blobWidth) {
+                            var text = node.textContent;
+                            var tabsCount = text.length - text.replace(/\t/g, "").length; // assume all tabs are at the beginning
+                            while (text.replace(/\t/g, "        ").length * charWidth >= blobWidth) {
+                                var span_1 = document.createElement("span");
+                                var cutLength = Math.floor(blobWidth / charWidth) - tabsCount * 7;
+                                span_1.innerText = text.substr(0, cutLength);
+                                text = text.substr(cutLength);
+                                node.parentNode.insertBefore(span_1, node);
+                                node.parentNode.insertBefore(document.createElement("br"), node);
+                            }
+                            var span = document.createElement("span");
+                            span.innerText = text;
+                            node.parentNode.replaceChild(span, node);
+                        }
+                        return true;
+                    }
+                    currentLength += charWidth * node.textContent.length;
+                }
+                else {
+                    var children = [];
+                    for (var i = 0; i < node.childNodes.length; ++i) {
+                        children.push(node.childNodes[i]);
+                    }
+                    for (var i = 0; i < children.length; ++i) {
+                        breakLine(children[i]);
+                    }
+                }
+                return false;
             }
-        }
-        return false;
-    }
-})(document);
+        }, 1000);
+    };
+    setInterval(func, 500);
+})(document, location);
--- main.ts
+++ main.ts
@@ -1,73 +1,86 @@
-((document: Document): void =>{
-	const blob = document.getElementsByClassName("blob-wrapper")[0] as HTMLDivElement
-	if(blob === undefined){
-		return
-	}
-	const blobWidth = (blob.clientWidth - document.getElementById("L1").clientWidth) * 0.85 // 85% to play safe
+((document: Document, location: Location): void =>{
+	let prevUrl = ""
 
-	const lineList = blob.getElementsByClassName("blob-code")
-
-	const charWidth = ((): number =>{
-		for(let i = 0; i < lineList.length; ++i){
-			const nodes = lineList[0].childNodes
-			for(let j = 0; j < nodes.length; ++j){
-				if(nodes[j] instanceof HTMLElement && (nodes[j] as HTMLElement).innerText.length > 0){
-					const node = nodes[j] as HTMLElement
-					return node.getBoundingClientRect().width / node.innerText.length
-				}
-			}
+	const func = (): void =>{
+		if(prevUrl === location.pathname){ // ignore line highlighting
+			return
 		}
-		return NaN
-	})()
-	if(isNaN(charWidth)){
-		return
-	}
+		prevUrl = location.pathname
 
-	let currentLength
+		setTimeout(() =>{
+			const blob = document.getElementsByClassName("blob-wrapper")[0] as HTMLDivElement
+			if(blob === undefined){
+				return
+			}
+			const blobWidth = (blob.clientWidth - document.getElementById("L1").clientWidth) * 0.85 // 85% to play safe
 
-	for(let i = 0; i < lineList.length; ++i){
-		currentLength = 0
-		breakLine(lineList[i] as HTMLTableCellElement)
-	}
+			const lineList = blob.getElementsByClassName("blob-code")
 
-	function breakLine(node: Node): boolean{
-		if(node instanceof Text){
-			const noTabsText = node.textContent.replace(/\t/g, "        ")
-			// actual number of spaces varies with .editorconfig, but let's assume it's 8 here, since we don't care getting too aggressive
-			if(currentLength + charWidth * noTabsText.length >= blobWidth){
-				if(currentLength > 0){
-					node.parentNode.insertBefore(document.createElement("br"), node)
-					currentLength = 0
-				}
-				if(charWidth * noTabsText.length >= blobWidth){
-					let text = node.textContent
-					const tabsCount = text.length - text.replace(/\t/g, "").length // assume all tabs are at the beginning
-
-					while(text.replace(/\t/g, "        ").length * charWidth >= blobWidth){
-						const span = document.createElement("span")
-						const cutLength = Math.floor(blobWidth / charWidth) - tabsCount * 7
-						span.innerText = text.substr(0, cutLength)
-						text = text.substr(cutLength)
-						node.parentNode.insertBefore(span, node)
-						node.parentNode.insertBefore(document.createElement("br"), node)
+			const charWidth = ((): number =>{
+				for(let i = 0; i < lineList.length; ++i){
+					const nodes = lineList[0].childNodes
+					for(let j = 0; j < nodes.length; ++j){
+						if(nodes[j] instanceof HTMLElement && (nodes[j] as HTMLElement).innerText.length > 0){
+							const node = nodes[j] as HTMLElement
+							return node.getBoundingClientRect().width / node.innerText.length
+						}
 					}
-					const span = document.createElement("span") as HTMLSpanElement
-					span.innerText = text
-					node.parentNode.replaceChild(span, node)
 				}
-				return true
+				return NaN
+			})()
+			if(isNaN(charWidth)){
+				return
 			}
-			currentLength += charWidth * node.textContent.length
-		}else{
-			const children = [] as Node[]
-			for(let i = 0; i < node.childNodes.length; ++i){
-				children.push(node.childNodes[i])
-			}
-			for(let i = 0; i < children.length; ++i){
-				breakLine(children[i])
-			}
-		}
 
-		return false
+			let currentLength
+
+			for(let i = 0; i < lineList.length; ++i){
+				currentLength = 0
+				breakLine(lineList[i] as HTMLTableCellElement)
+			}
+
+			function breakLine(node: Node): boolean{
+				if(node instanceof Text){
+					const noTabsText = node.textContent.replace(/\t/g, "        ")
+					// actual number of spaces varies with .editorconfig, but let's assume it's 8 here, since we don't care getting too aggressive
+					if(currentLength + charWidth * noTabsText.length >= blobWidth){
+						if(currentLength > 0){
+							node.parentNode.insertBefore(document.createElement("br"), node)
+							currentLength = 0
+						}
+						if(charWidth * noTabsText.length >= blobWidth){
+							let text = node.textContent
+							const tabsCount = text.length - text.replace(/\t/g, "").length // assume all tabs are at the beginning
+
+							while(text.replace(/\t/g, "        ").length * charWidth >= blobWidth){
+								const span = document.createElement("span")
+								const cutLength = Math.floor(blobWidth / charWidth) - tabsCount * 7
+								span.innerText = text.substr(0, cutLength)
+								text = text.substr(cutLength)
+								node.parentNode.insertBefore(span, node)
+								node.parentNode.insertBefore(document.createElement("br"), node)
+							}
+							const span = document.createElement("span") as HTMLSpanElement
+							span.innerText = text
+							node.parentNode.replaceChild(span, node)
+						}
+						return true
+					}
+					currentLength += charWidth * node.textContent.length
+				}else{
+					const children = [] as Node[]
+					for(let i = 0; i < node.childNodes.length; ++i){
+						children.push(node.childNodes[i])
+					}
+					for(let i = 0; i < children.length; ++i){
+						breakLine(children[i])
+					}
+				}
+
+				return false
+			}
+		}, 1000)
 	}
-})(document)
+
+	setInterval(func, 500)
+})(document, location)
